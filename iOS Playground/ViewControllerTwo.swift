70fef9564cd344142db0c352879886c7e9d4fdfa
Evan Emelga <evanemelga@hotmail.com>
2019-05-08 13:10:39 +0700

    Programmatically present and dismiss

--- iOS Playground/ViewControllerTwo.swift
+++ iOS Playground/ViewControllerTwo.swift
@@ -12,12 +12,26 @@ class ViewControllerTwo: UIViewController {
 
     override func viewDidLoad()
     {
+        addNavItem()
         super.viewDidLoad()
-        
     }
 
     @IBAction func goBack(_ sender: UIButton)
     {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func addNavItem()
+    {
+        let navbar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: UIScreen.main.bounds.size.width,height: 50));
+        navbar.tintColor = UIColor.lightGray
+        self.view.addSubview(navbar)
+        
+        let navItem = UINavigationItem(title: "Second Screen")
+        let navBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.undo, target: self, action: nil)
+        //let navBarButton = UIBarButtonItem(title: "<Back", style: .done, target: self, action: nil)
+        navItem.leftBarButtonItem = navBarButton
+        
+        navbar.items = [navItem]
     }
 }
