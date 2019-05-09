b60712a33d18df54e64578dcd414447edf8693a1
Evan Emelga <evanemelga@hotmail.com>
2019-05-09 21:07:23 +0700

    Sectioned Table View

--- iOS Playground/Controllers/TableViewController.swift
+++ iOS Playground/Controllers/TableViewController.swift
@@ -11,13 +11,29 @@ import UIKit
 class TableViewController: UIViewController
 {
     @IBOutlet weak var tableView: UITableView!
-    var items: [String] = ["Test","😀","😇","😚","😫","🤬","🥶","😱","😨","🤢","🤝","✍️","👤","👴","👨‍⚕️"]
+    var students: [Student] = [
+        Student("😀",1),
+        Student("😇",1),
+        Student("😚",2),
+        Student("😫",2),
+        Student("🤬",3),
+        Student("🥶",3),
+        Student("😱",1),
+        Student("😨",1),
+        Student("🤢",2),
+        Student("🤝",2),
+        Student("✍️",3),
+        Student("👤",3),
+        Student("👴",1),
+        Student("👨‍⚕️",1)]
+    
+    var lastIndex: Int?
+    var studentsInThisSection: [Student] = []
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
         tableView.dataSource = self
     }
 
@@ -25,17 +41,37 @@ class TableViewController: UIViewController
 
 extension TableViewController: UITableViewDataSource
 {
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        let batches = self.students.map { $0.batch }
+        let distinct = Array(Set(batches))
+        return distinct.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return self.items.count
+        return self.students.filter({ (Q) -> Bool in
+            Q.batch == section + 1
+        }).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        let item = self.items[indexPath.item]
-        cell.textLabel?.text = item
+        if self.lastIndex != indexPath.section + 1 {
+            self.studentsInThisSection = self.students.filter({ (Q) -> Bool in
+                Q.batch == indexPath.section + 1
+            })
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        let item = self.studentsInThisSection[indexPath.row]
+        cell.textLabel?.text = item.face
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        return "Batch \(section + 1)"
     }
 }
---
+++ iOS Playground/Models/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  iOS Playground
+//
+//  Created by Evan Emelga on 09/05/19.
+//  Copyright © 2019 Evan Emelga. All rights reserved.
+//
+
+import Foundation
+
+class Student
+{
+    var face: String
+    var batch: Int
+    
+    init(_ face: String, _ batch: Int) {
+        self.face = face
+        self.batch = batch
+    }
+}
